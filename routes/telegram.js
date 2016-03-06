504bd923235640df6af53eff7df6a0810c641f76
Beto Sosa <betososah@gmail.com>
2016-03-06 00:54:38 -0600

    Google storage

--- routes/telegram.js
+++ routes/telegram.js
@@ -2,21 +2,41 @@ var express = require('express');
 var router = express.Router();
 var telegram = require('../helpers/telegram.js');
 
+var caption = require('caption');
+var multer = require('multer');
+var upload = multer({ dest: './uploads/' });
+
+var gcloud = require('gcloud');
+
+var gcs = gcloud.storage({
+  projectId: 'missyapp-1242',
+  keyFilename: './config/missyapp-c13275e43cdc.json'
+});
+
+var bucket = gcs.bucket('missyappbucket');
+
 router.post('/', function(req, res, next) {
   var update = req.body;
-  console.log('----------');
-  console.log(update);
-  console.log('----------');
   var telegramCmd = update.message.text;
   console.log(telegramCmd);
   var chat_id = update.message.chat.id;
   var reply_to_message_id = update.message.message_id;
   if (telegramCmd === '/hola') {
-    console.log('----------*************************');
     telegram.sendMessage(chat_id, 'hola');
-    //res.json({ status: 'ok' });
   }
   res.json({ status: 'ok' });
+});
+
+router.post('/image', upload.single('missing'), function(req, res, next) {
+  caption.path(req.file.path, {caption: 'HHOOLLAA', outputFile: './uploads/'+req.file.originalname}, function(err, image) {
+    if (err) return err;
+    bucket.upload('./uploads/'+req.file.originalname, function(err, file) {
+      if (err) {
+        return res.json({ error: err });
+      }
+      res.json({status: 'ok'});
+    });
+  })
 });
 
 module.exports = router;
