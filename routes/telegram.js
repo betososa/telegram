f91682e607ce0161ceb457b9ddc8e73d28606a3f
Beto Sosa <betososah@gmail.com>
2016-03-05 21:07:05 -0600

    First command

--- routes/telegram.js
+++ routes/telegram.js
@@ -1,8 +1,16 @@
 var express = require('express');
 var router = express.Router();
+var telegram = require('../helpers/telegram.js');
 
 router.post('/', function(req, res, next) {
-  console.log(req);
+  var update = req.body;
+  console.log(update);
+  var telegramCmd = body.text;
+  var chat_id = telegramUpdate.message.chat.id;
+  var reply_to_message_id = telegramUpdate.message.message_id;
+  if (telegramCmd === '/hola') {
+    telegram.sendMessage(chat_id, 'hola', reply_to_message_id);
+  }
   res.json({ status: 'ok' });
 });
 
